e013ecad906c00c9603bcfb5c0464144edad0f6a
Gavin Morris <gavin@almostpurple.com>
2015-05-15 13:25:10 +0800

    initial commit

---
+++ NetTcpClient/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NetTcpClient
+{
+  [DataContract]
+  class SecurityAuthorisationFailure { }
+
+  [ServiceContract]
+  interface IService1
+  {
+    [OperationContract, FaultContract(typeof(SecurityAuthorisationFailure))]
+    void Foo();
+  }
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var host = (args.Length > 0 ? args[0] : Environment.MachineName) + ":9170";
+      var target = "net.tcp://" + host;
+
+      var binding = new NetTcpBinding(SecurityMode.Transport);
+      binding.Security.Transport.ClientCredentialType = TcpClientCredentialType.Windows;
+      binding.Security.Transport.ProtectionLevel = System.Net.Security.ProtectionLevel.EncryptAndSign;
+      /*binding.ReliableSession.Enabled = true;
+      binding.ReliableSession.InactivityTimeout = TimeSpan.FromSeconds(60);
+      binding.ReliableSession.Ordered = false;*/
+      //binding.TransferMode = TransferMode.Streamed;
+
+      var epid = EndpointIdentity.CreateDnsIdentity(host);
+      var ep = new EndpointAddress(new Uri(target), epid);
+
+      while (true)
+      {
+        ChannelFactory<IService1> factory = null;
+
+        try
+        {
+          factory = new ChannelFactory<IService1>(binding, ep);
+          //factory.Credentials.Windows.ClientCredential = new NetworkCredential("karisma", "karisma", "kestral");
+
+          var client = factory.CreateChannel();
+          try
+          {
+            client.Foo();
+          }
+          catch (FaultException<SecurityAuthorisationFailure>)
+          {
+            factory.Abort();
+            factory.Close();
+
+            Console.WriteLine("Failed, retry as karisma");
+            
+            factory = new ChannelFactory<IService1>(binding, ep);
+            factory.Credentials.Windows.ClientCredential = new NetworkCredential("karisma", "karisma", "kestral");
+
+            client = factory.CreateChannel();
+            try
+            {
+              client.Foo();
+            }
+            catch
+            {
+              Console.WriteLine("FAILED");
+            }
+          }
+        }
+        finally
+        {
+          factory.Close();
+        }
+
+        Thread.Sleep(200);
+      }
+
+      Console.WriteLine("Done. Press enter ...");
+      Console.ReadLine();
+    }
+  }
+}
---
+++ NetTcpServer/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetTcpServer
+{
+  [DataContract]
+  class SecurityAuthorisationFailure { }
+
+  [ServiceContract]
+  interface IService1
+  {
+    [OperationContract, FaultContract(typeof(SecurityAuthorisationFailure))]
+    void Foo();
+  }
+
+  [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single, ConcurrencyMode = ConcurrencyMode.Multiple, UseSynchronizationContext = false)]
+  class Service1 : IService1
+  {
+    public void Foo()
+    {
+      var name = OperationContext.Current.ServiceSecurityContext.PrimaryIdentity.Name;
+
+      Console.WriteLine("{0}\tCall from {1}", DateTime.Now, name);
+      
+      if (name.Equals("kestral\\gavinm", StringComparison.CurrentCultureIgnoreCase)) 
+        throw new FaultException<SecurityAuthorisationFailure>(new SecurityAuthorisationFailure(), "lalala");
+      
+      //throw new NotImplementedException();
+    }
+  }
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var host = (args.Length > 0 ? args[0] : Environment.MachineName) + ":9170";
+      var target = "net.tcp://" + host;
+
+      var binding = new NetTcpBinding(SecurityMode.Transport);
+      binding.Security.Transport.ClientCredentialType = TcpClientCredentialType.Windows;
+      binding.Security.Transport.ProtectionLevel = System.Net.Security.ProtectionLevel.EncryptAndSign;
+      /*binding.ReliableSession.Enabled = true;
+      binding.ReliableSession.InactivityTimeout = TimeSpan.FromSeconds(60);
+      binding.ReliableSession.Ordered = false;*/
+      //binding.TransferMode = TransferMode.Streamed;
+
+      var serviceHost = new ServiceHost(new Service1());
+      serviceHost.AddServiceEndpoint(typeof(IService1), binding, target);
+      serviceHost.Credentials.WindowsAuthentication.AllowAnonymousLogons = false;
+
+      /*var throttlingBehavior = serviceHost.Description.Behaviors.Find<ServiceThrottlingBehavior>();
+      if (throttlingBehavior == null)
+      {
+        throttlingBehavior = new ServiceThrottlingBehavior();
+        serviceHost.Description.Behaviors.Add(throttlingBehavior);
+      }
+      //ServiceThrottlingBehavior.MaxConcurrentSessions = 2;
+      */
+
+
+      serviceHost.Open();
+
+      Console.WriteLine("Listening on " + target);
+      Console.WriteLine("Press enter to close ...");
+      Console.ReadLine();
+
+      serviceHost.Close();
+    }
+  }
+}
+

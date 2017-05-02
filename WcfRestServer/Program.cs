d6306aa7609617a7e0e0dee147917c27de32a622
Gavin Morris <gavin@almostpurple.com>
2017-05-02 11:02:29 +0800

    add client cert

--- WcfRestServer/Program.cs
+++ WcfRestServer/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Selectors;
+using System.IO;
 using System.Linq;
 using System.Runtime.Serialization;
+using System.Security.Cryptography.X509Certificates;
 using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
@@ -22,9 +25,28 @@ namespace WcfRestServer
   {
     [WebGet(UriTemplate = "/query/{query}", ResponseFormat = WebMessageFormat.Json)]
     Metadata[] Query(string query);
+    [WebInvoke(UriTemplate = "/process")]
+    void Process(Stream request);
   }
   class TestService : ITestService
   {
+
+    public void Process(Stream request)
+    {
+      var reader = new StreamReader(request);
+      var text = reader.ReadToEnd();
+
+      var r = new Random();
+      if (r.Next(2) == 0)
+      {
+        //throw new NotImplementedException();
+        WebOperationContext.Current.OutgoingResponse.StatusCode = System.Net.HttpStatusCode.NotImplemented;
+        return;
+      }
+
+      Console.WriteLine(text);
+    }
+
     public Metadata[] Query(string query)
     {
       return new[] {
@@ -39,12 +61,31 @@ namespace WcfRestServer
     {
       Console.Write("Starting ...");
 
+      var address = new Uri("https://localhost:5000");
+
+      var binding = new WebHttpBinding();
+      binding.Security.Mode = WebHttpSecurityMode.Transport;
+      binding.Security.Transport.ClientCredentialType = HttpClientCredentialType.Certificate;
+
       var provider = new TestService();
-      var host = new WebServiceHost(typeof(TestService), new Uri("http://localhost:5000"));
+      var host = new WebServiceHost(typeof(TestService), address);
+      var endpoint = host.AddServiceEndpoint(typeof(ITestService), binding, address);
+
+      host.Credentials.ClientCertificate.Authentication.CertificateValidationMode = System.ServiceModel.Security.X509CertificateValidationMode.Custom;
+      host.Credentials.ClientCertificate.Authentication.CustomCertificateValidator = new CustomCertValidator();
+
       host.Open();
       
       Console.WriteLine(" ready!");
       Console.ReadLine();
+    }
+  }
+  class CustomCertValidator : X509CertificateValidator
+  {
+    public override void Validate(X509Certificate2 certificate)
+    {
+      // throw System.IdentityModel.Tokens.SecurityTokenvalidationException to reject
+      return;
     }
   }
 }
